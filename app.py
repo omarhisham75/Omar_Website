f8d6a49f1d8b245592c0e2dcc05de7d3349cb6df
Omar Hisham <62573809+omarhisham75@users.noreply.github.com>
2024-07-13 17:08:56 +0000

    to be continued

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , jsonify
 
 app = Flask(__name__)
 
+Jobs = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Cairo,Egypt',
+    'salary': '$1,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Cairo,Egypt',
+    'salary': '$1,500'
+  },
+  {
+    'id': 3,
+    'title': 'Mechanical Engineer',
+    'location': 'Cairo,Egypt',
+    'salary': '$2,000'
+  },
+  {
+    'id': 4,
+    'title': 'Software Engineer',
+    'location': 'Cairo,Egypt',
+    'salary': '$2,000'
+  }
+]
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=Jobs)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(Jobs)
 
 
 if __name__ == "__main__":
